57e2c52a5f03b842bfa190f620964a166ae24594
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-21 15:04:26 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function sortReverse(){
+	var a = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+
+	console.log("Sebelumnya : ", a)
+	console.log("Ascending : ", a.sort())
+    console.log("Descending : ", a.reverse())
+}
+
+sortReverse();
